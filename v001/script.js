fb754b07ea0b6c905def92a4c5224b32bb83b36e
Erico Cerone <59001734+ericocerone@users.noreply.github.com>
2023-06-10 16:28:25 -0300

    Add files via upload

---
+++ v001/script.js
@@ -0,0 +1,112 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startGame() {
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Reiniciar'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [
+    {
+        question: 'Quanto é 2 + 2?',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    },
+    {
+        question: 'Qual é a capital do Brasil?',
+        answers: [
+            { text: 'Brasília', correct: true },
+            { text: 'São Paulo', correct: false },
+            { text: 'Rio de Janeiro', correct: false },
+            { text: 'Salvador', correct: false }
+        ]
+    },
+    {
+        question: 'O que é HTML?',
+        answers: [
+            { text: 'Linguagem de programação', correct: false },
+            { text: 'Linguagem de marcação (Hypertext Markup Language)', correct: true },
+            { text: 'Abreviação para "How To Meet Ladies"', correct: false },
+            { text: 'Remédio de pingar no nariz', correct: false }
+        ]
+    },
+    {
+        question: 'Qual é a raiz quadrada de 49?',
+        answers: [
+            { text: '9', correct: false },
+            { text: '7', correct: true },
+        ]
+    }
+]
